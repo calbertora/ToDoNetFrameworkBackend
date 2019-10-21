ad06860cfbefe62b95cc6a4b5ff9cb3c92926234
Carlos Ramírez <calbertora@gmail.com>
2019-10-21 13:06:07 -0500

    Add the Get and Post methods

---
+++ ToDoNetFrameworkBackend/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoNetFrameworkBackend.DAL;
+using ToDoNetFrameworkBackend.Models;
+
+namespace ToDoNetFrameworkBackend.Controllers
+{
+    public class TaskController : ApiController
+    {
+        private Repository reposiotry;
+
+        public IEnumerable<TaskToDo> Get()
+        {
+            return reposiotry.GetAllTask();
+        }
+
+        public void Post(TaskToDo task)
+        {
+            reposiotry.InsertTask(task);
+        }
+    }
+}
